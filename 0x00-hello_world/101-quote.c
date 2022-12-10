224599445f54bd32010f7fed16256dd9d1ded39e
Imhandegbelo <okogbe@outlook.com>
2022-12-09 23:22:21 -0800

    final advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: print to stdout using write funtion
+ * Return: 1 (error)
+*/
+
+int main(void)
+{
+	char tx[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, tx, 59);
+	return (1);
+}
