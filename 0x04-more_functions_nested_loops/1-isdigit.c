1f43e03149bac9b5e0668b838ef2fc927ff91a41
Imhandegbelo <okogbe@outlook.com>
2022-12-15 20:21:26 -0800

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,8 +3,7 @@
 /**
  * _isdigit - function that checks for a digit
  * @c: expected input to check
- * Return: 1 if c is digit
- * 		0 otherwise
+ * Return: 1 if c is digit 0 otherwise
  */
 
 int _isdigit(int c)
