36782e3960c1b73331948496385498fd152a6da8
Imhandegbelo <abramgeo5@gmail.com>
2023-01-05 02:46:26 -0800

    update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,17 @@
+/**
+ * puts2 - prints all odd characters of a string beginings from *[0]
+ * @str: string to analyse
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i%2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
