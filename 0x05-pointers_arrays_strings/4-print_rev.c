b6232c634750004ec11541a7d0f92d0aa76cc535
Imhandegbelo <abramgeo5@gmail.com>
2023-01-05 02:34:52 -0800

    update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+/**
+ * print_rev - prints a string in reverse order
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i != 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
