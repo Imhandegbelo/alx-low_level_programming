c93d4b371d9deee03e4c410bc2dcc61383573be1
Imhandegbelo <abramgeo5@gmail.com>
2023-01-05 03:08:01 -0800

    update 7-puts-half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+/**
+ * puts-half - string is even? print half of it : print (length-1)/2 of it
+ * @str: input string
+ */
+
+void puts_half(char *str)
+{
+	int length_of_the_string = strlen(str);
+	int half;
+
+	if (length_of_the_string % 2 == 0)
+		half = length_of_the_string / 2;
+	else
+		half = (length_of_the_string - 1) / 2;
+
+	while (*(str + half) != '\0')
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
