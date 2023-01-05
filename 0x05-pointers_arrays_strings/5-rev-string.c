185ba89ac9f6d6261b4a3abd7c99bb2260f20dbb
Imhandegbelo <abramgeo5@gmail.com>
2023-01-05 02:41:02 -0800

    update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev-string.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+/**
+ * rev_string - function that reverses a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	char new_s;
+
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	*s = new_s;
+}
