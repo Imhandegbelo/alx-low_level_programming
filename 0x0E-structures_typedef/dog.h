22860a305efe0b40adf18e382e82ad392aec720d
Imhandegbelo <abramgeo5@gmail.com>
2023-01-17 21:31:56 -0800

    fix: repositioned the function call

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 /**
  * struct dog - structure of dogs
@@ -19,7 +14,8 @@ struct dog
 	char  *name;
 	float age;
 	char *owner;
-}
+};
 
+typedef struct dog dog_t;
 
 #endif /*DOG_H*/
