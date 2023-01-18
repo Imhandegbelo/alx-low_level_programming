bfaa91291b46cf9a101aa8ca40a8b77f90c5ee61
Imhandegbelo <abramgeo5@gmail.com>
2023-01-17 22:51:59 -0800

    fix: indluded stdlib to header file && updated 5. free dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: pointer to name
+ * @age: age
+ * @owner: pointer to dog owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
