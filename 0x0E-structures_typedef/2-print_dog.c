95411a0aa0cfd2ac6b4538bfa55b05f8fee1e708
Imhandegbelo <abramgeo5@gmail.com>
2023-01-17 22:33:36 -0800

    fix: function name

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,9 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	(d.name == NULL) ? printf("Name: (nil)\n")
-		: printf("Name: %s\n", d.name);
-	printf("Age: %f\n", d.age);
-	(d.owmner == NULL) ? printf("Owner: (nil)\n")
-		: prinf("Owner: %s\n", d.owner);
+	(d->name == NULL) ? printf("Name: (nil)\n")
+		: printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	(d->owmner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
 }
