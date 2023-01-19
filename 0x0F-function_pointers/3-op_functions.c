87096a9a9ed3fe8a7ee5d621b45e732de34a44d2
Imhandegbelo <abramgeo5@gmail.com>
2023-01-18 20:19:33 -0800

    update: 3. op_functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - performs addition
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - performs subtraction
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - performs multiplication
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - performs division
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - performs reminder division
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
