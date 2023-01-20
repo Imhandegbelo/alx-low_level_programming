8ec079edddf11d5f7b502e973bb81927e53ce76a
Imhandegbelo <abramgeo5@gmail.com>
2023-01-19 20:32:54 -0800

    fix: 2. print stings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(list, char *);
 
 		if (str == NULL)
-			str = "(nil)";
+			printf("(nil)");
 		printf("%s", str);
 		if (i != (n - 1) && str != NULL)
 			printf("%s", separator);
