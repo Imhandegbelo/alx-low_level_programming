d52c641217dda5372c0499db0909aa8232529cf0
Imhandegbelo <abramgeo5@gmail.com>
2023-04-13 20:33:59 -0700

    added shared files

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	/**
+	 * declare unsigned int because the value is
+	 * expected to be non-zero always
+	 */
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
